7f3a67cfd7938b3a93b1a974790d3ce9f94531a8
Uriel Larrea <kuroshiromugen@gmail.com>
2024-09-12 18:35:09 -0300

    Moved whole project to quick start maven archetype, removed old homework main demo code

---
+++ Maven Hierarchy Exercise/src/Main.java
@@ -0,0 +1,10 @@
+import Domain.*;
+import Exceptions.*;
+import org.apache.logging.log4j.Logger;
+public class Main {
+    private static Logger logger;
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
